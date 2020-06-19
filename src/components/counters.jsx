5697e1e5353ea0b9b82ff6593ab659273315962e
biagemc <biagemc@gmail.com>
2020-06-18 22:35:29 -0400

    Refactoring props on all components

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,18 +3,14 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
+    const { onreset, counters, onIncrement, onDelete } = this.props;
     return (
       <div>
-        <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
           Reset
         </button>
-        {this.props.counters.map((counter) => (
-          <Counter
-            key={counter.id}
-            onIncrement={this.props.onIncrement}
-            onDelete={this.props.onDelete}
-            counter={counter}
-          />
+        {counters.map((counter) => (
+          <Counter key={counter.id} onIncrement={onIncrement} onDelete={onDelete} counter={counter} />
         ))}
       </div>
     );
